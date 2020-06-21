215ef0f539d3aaafd4a92856ac9bbbff5523b510
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-06-21 22:46:20 +0300

    redone structure

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import { createStore } from 'redux';
 
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INC':
-      return state + 1;
-
-    default:
-      return state;
-  }
-  return 0;
-};
+import reducer from './reducer';
+import { inc, dec, rnd } from './actions';
 
 const store = createStore(reducer);
 
-store.subscribe(() => {
-  console.log(store.getState());
-});
+document
+  .getElementById('inc')
+  .addEventListener('click', () => {
+    store.dispatch(inc());
+  });
 
-store.dispatch({ type: 'INC' });
-store.dispatch({ type: 'INC' });
+document
+  .getElementById('dec')
+  .addEventListener('click', () => {
+    store.dispatch(dec());
+  });
+
+document
+  .getElementById('rnd')
+  .addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 10);
+    store.dispatch(rnd(payload));
+  });
+
+const update = () => {
+  document
+    .getElementById('counter')
+    .innerHTML = store.getState();
+
+};
+
+store.subscribe(update);
