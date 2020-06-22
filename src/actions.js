7a452546bdcb72a71a81dc1916bb179e94fa08e7
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-06-22 12:30:03 +0300

    fixed

--- src/actions.js
+++ src/actions.js
@@ -5,6 +5,6 @@ export const dec = () => ({ type: 'DEC' });
 export const rnd = (payload) => {
     return {
         type: 'RND',
-        payload: Math.floor(Math.random() * 10);
+        payload: Math.floor(Math.random() * 10)
     }
 };
