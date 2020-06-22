ad1d4cb3d52d2918ff42f4a3594f4713c64594bc
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-06-22 11:38:49 +0300

    added react-redux connect

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const Counter = ({ counter, inc, dec, rnd }) => {
     return (
@@ -11,4 +12,10 @@ const Counter = ({ counter, inc, dec, rnd }) => {
     );
 };
 
-export default Counter;
+const mapStateToProps = (state) => {
+    return {
+        counter: state
+    };
+};
+
+export default connect(mapStateToProps)(Counter);
